172d4ef94ba1954320f8170f9496f395c86f15e5
limjh <jhlim@devguru.co.kr>
2019-03-27 09:13:33 +0900

    day8

---
+++ Bowling/Day8/BowlingKata/BowlingKata.Test/UnitTest1.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BowlingKata.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void 클래스_인스턴스_생성()
+        {
+            var game = new Game();
+        }
+
+        [TestMethod]
+        public void roll_score_함수생성()
+        {
+            var game = new Game();
+
+            game.roll(1);
+
+            Assert.AreEqual(1, game.score());
+        }
+
+        [TestMethod]
+        public void _3ball_1spare()
+        {
+            var game = new Game();
+
+            game.roll(5);
+            game.roll(5);
+            game.roll(5);
+
+            Assert.AreEqual(20, game.score());
+        }
+
+        [TestMethod]
+        public void random_test_01()
+        {
+            var game = new Game();
+
+            game.roll(5);
+            game.roll(2);
+
+            Assert.AreEqual(7, game.score());
+        }
+
+        [TestMethod]
+        public void random_test_02()
+        {
+            var game = new Game();
+
+            game.roll(5);
+            game.roll(2);
+            game.roll(9);
+            game.roll(1);
+            game.roll(3);
+            game.roll(2);
+
+            Assert.AreEqual(25, game.score());
+        }
+
+        [TestMethod]
+        public void random_test_03()
+        {
+            var game = new Game();
+
+            game.roll(5);
+            game.roll(2);
+            game.roll(9);
+            game.roll(1);
+            game.roll(3);
+            game.roll(2);
+            game.roll(10);
+
+            Assert.AreEqual(35, game.score());
+        }
+
+        [TestMethod]
+        public void random_test_04()
+        {
+            var game = new Game();
+
+            game.roll(5);
+            game.roll(2);
+            game.roll(9);
+            game.roll(1);
+            game.roll(3);
+            game.roll(2);
+            game.roll(10);
+            game.roll(5);
+            game.roll(0);
+
+            Assert.AreEqual(45, game.score());
+        }
+
+        [TestMethod]
+        public void random_test_05()
+        {
+            var game = new Game();
+
+            game.roll(5);
+            game.roll(2);
+
+            game.roll(9);
+            game.roll(1);
+
+            game.roll(3);
+            game.roll(2);
+
+            game.roll(10);
+
+            game.roll(5);
+            game.roll(0);
+
+            game.roll(10);
+
+            game.roll(10);
+
+            game.roll(10);
+
+            game.roll(8);
+            game.roll(1);
+
+            Assert.AreEqual(131, game.score());
+        }
+
+        [TestMethod]
+        public void random_test_06()
+        {
+            var game = new Game();
+
+            game.roll(5);
+            game.roll(2);
+
+            game.roll(9);
+            game.roll(1);
+
+            game.roll(3);
+            game.roll(2);
+
+            game.roll(10);
+
+            game.roll(5);
+            game.roll(0);
+
+            game.roll(10);
+
+            game.roll(10);
+
+            game.roll(10);
+
+            game.roll(8);
+            game.roll(1);
+
+            game.roll(5);
+            game.roll(5);
+            game.roll(4);
+
+            Assert.AreEqual(145, game.score());
+        }
+
+        [TestMethod]
+        public void random_test_07()
+        {
+            var game = new Game();
+
+            game.roll(10);
+
+            game.roll(10);
+
+            game.roll(10);
+
+            game.roll(10);
+
+            game.roll(10);
+
+            game.roll(10);
+
+            game.roll(10);
+
+            game.roll(10);
+
+            game.roll(10);
+
+            game.roll(10);
+            game.roll(10);
+            game.roll(10);
+
+            Assert.AreEqual(300, game.score());
+        }
+
+        [TestMethod]
+        public void random_test_08()
+        {
+            var game = new Game();
+
+            game.roll(5);
+            game.roll(5);
+
+            game.roll(5);
+            game.roll(5);
+
+            game.roll(5);
+            game.roll(5);
+
+            game.roll(5);
+            game.roll(5);
+
+            game.roll(5);
+            game.roll(5);
+
+            game.roll(5);
+            game.roll(5);
+
+            game.roll(5);
+            game.roll(5);
+
+            game.roll(5);
+            game.roll(5);
+
+            game.roll(5);
+            game.roll(5);
+
+            game.roll(5);
+            game.roll(5);
+            game.roll(5);
+
+            Assert.AreEqual(150, game.score());
+        }
+    }
+}
---
+++ Bowling/Day8/BowlingKata/BowlingKata/Game.cs
@@ -0,0 +1,102 @@
+﻿using System;
+
+namespace BowlingKata
+{
+    public class Game
+    {
+        private int ballCnt;
+        private HalfFrame[] frames;
+
+        public Game()
+        {
+            ballCnt = 0; ;
+            frames = new HalfFrame[21];
+        }
+
+        public void roll(int pin)
+        {
+            frames[ballCnt].pin = pin;
+
+            if (CheckSpare(pin))
+            {
+                frames[ballCnt].isSpare = true;
+            }
+            else if (CheckStrike(pin))
+            {
+                frames[ballCnt].isStrike = true;
+
+                if (ballCnt < 18)
+                    ballCnt++;
+            }
+            else if (CheckGutter(pin))
+            {
+                frames[ballCnt].isGutter = true;
+            }
+
+            ballCnt++;
+        }
+
+        public int score()
+        {
+            int totalScore = 0;
+
+            for (int i = 0; i < frames.Length; i++)
+            {
+                if (frames[i].isSpare && i < 18)
+                {
+                    AddSpareBonus(i);
+                }
+                else if (frames[i].isStrike && i < 18)
+                {
+                    AddStrikeBonus(i);
+                }
+
+                totalScore += frames[i].pin;
+            }
+
+            return totalScore;
+        }
+
+        private void AddStrikeBonus(int i)
+        {
+            int addCnt = 2;
+            for (int j = i + 2; j < frames.Length; j++)
+            {
+                if (addCnt > 0 && (frames[j].pin != 0 || frames[j].isGutter))
+                {
+                    frames[i].pin += frames[j].pin;
+                    addCnt--;
+                }
+            }
+        }
+
+        private void AddSpareBonus(int i)
+        {
+            frames[i].pin += frames[i + 1].pin;
+        }
+
+        private bool CheckGutter(int pin)
+        {
+            return pin == 0;
+        }
+
+        private bool CheckStrike(int pin)
+        {
+            return ballCnt % 2 == 0 &&
+                            pin == 10;
+        }
+
+        private bool CheckSpare(int pin)
+        {
+            return ballCnt % 2 == 1 && pin + frames[ballCnt - 1].pin == 10;
+        }
+    }
+
+    public struct HalfFrame
+    {
+        public int pin;
+        public bool isSpare;
+        public bool isStrike;
+        public bool isGutter;
+    }
+}
