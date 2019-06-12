8260adb5d3f734deadae78b6a14a8efb0cfdfb70
limjh <jhlim@devguru.co.kr>
2019-06-12 13:35:24 +0900

    Day2

--- RegExp/Day1/RegularExp/RegEx.cs
+++ RegExp/Day1/RegularExp/RegEx.cs
@@ -11,6 +11,12 @@ namespace RegularExp
     {
         public bool matching(string input, string pattern)
         {
+            ArrayList inputArrayList = stringToArrayList(input);
+            ArrayList patternArrayList = stringToArrayList(pattern);
+
+            
+
+
             return false;
         }
 
@@ -18,9 +24,23 @@ namespace RegularExp
         {
             ArrayList output = new ArrayList();
 
-
+            List<Char> list = input.ToList();
+            foreach(Char s in list)
+            {
+                output.Add(s.ToString());
+            }
 
             return output;
+        }
+
+        public bool isContainStar(ArrayList list)
+        {
+            foreach (string c in list)
+            {
+                if (c.Contains("*"))
+                    return true;
+            }
+            return false;
         }
     }
 }
