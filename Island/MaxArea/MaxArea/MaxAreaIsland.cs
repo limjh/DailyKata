910c78eaff0b9301ae01c758aa7147f89908b17e
limjh <jhlim@devguru.co.kr>
2019-10-31 09:00:22 +0900

    a

--- Island/MaxArea/MaxArea/MaxAreaIsland.cs
+++ Island/MaxArea/MaxArea/MaxAreaIsland.cs
@@ -1,66 +1,398 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
+/*
+{0,1,0}
+{1,1,1}
+{0,1,0}
+
+make itemConditionMap
+
+000  | 011B     | 020
+100R | 111LRTB  | 121L
+200  | 211T     | 220
+
+00 : SKIP(0)
+
+01 : ADD, delete (first add. no check from), delete (to B), check, jump (B)
+
+11 : ADD, delete (from T), delete (to L), check, jump (L)
+
+10 : ADD, delete (from R), check no jump site >
+
+check list's item(01, 11, 10) have LRTB > 11 Have R, B > delete (to R)
+
+12 : ADD, delete (from L), check no jump site >
+
+check list's item(01, 10, 11, 12) have LRTB > 11 Have B > delete (to B)
+
+21 : ADD, delete(T), check no jump site > list clean > break;
+
+01 : SKIP(isChecked)
+
+02 : SKIP(0)
+
+10 : SKIP(isChecked)
+
+11 : SKIP(isChecked)
+...
+...
+     */
+
 namespace MaxArea
 {
-    public class ItemInfo
+    public class ItemConditionInfo
     {
-        public bool isLeft = false;
-        public bool isRight = false;
-        public bool isTop = false;
-        public bool isBottom = false;
+        /*
+         * index- mean
+        0       - rowIndex (int) 
+        1       - colIndex (int) 
+        2       - value (int)
+        3       - isChecked(bool)
+        4       - value of 1 on the left (string L)
+        4|5     - value of 1 on the right (string R)
+        4|5|6   - value of 1 on the top (string T)
+        4|5|6|7 - value of 1 on the bottom (string B)
+        */
+
+        public ArrayList info = new ArrayList();
+
+        public void SetIndexInfo(int rowIndex, int colmnIndex, int value)
+        {
+            bool isChecked = false;
+            info.Add(rowIndex);
+            info.Add(colmnIndex);
+            info.Add(value);
+            info.Add(isChecked);
+        }
+        public void SetItemIsChecked()
+        {
+            info.Remove(false);
+            info.Insert(3, true);
+        }
+        public void SetAroundItemExist(bool left, bool right, bool top, bool bottom)
+        {
+            if (left)   info.Add("L");
+            if (right)  info.Add("R");
+            if (top)    info.Add("T");
+            if (bottom) info.Add("B");
+        }
+        public int getRowIndex()
+        {
+            if (info.Count < 1)
+                return -1;
+
+            return (int)info[0];
+        }
+
+        public int getColIndex()
+        {
+            if (info.Count < 2)
+                return -1;
+
+            return (int)info[1];
+        }
+
+        public int getValue()
+        {
+            if (info.Count < 3)
+                return -1;
+
+            return (int)info[2];
+        }
+        public bool isChecked()
+        {
+            return (bool)info[3];
+        }
+
+        public bool isExistArroundIsland()
+        {
+            if (info.Contains("L") || 
+                info.Contains("R") || 
+                info.Contains("T") || 
+                info.Contains("B"))
+                return true;
+
+            return false;
+        }
+
+        public bool isExistLeftIsland()
+        {
+            if (info.Contains("L"))
+                return true;
+
+            return false;
+        }
+        public bool isExistRightIsland()
+        {
+            if (info.Contains("R"))
+                return true;
+
+            return false;
+        }
+        public bool isExistTopIsland()
+        {
+            if (info.Contains("T"))
+                return true;
+
+            return false;
+        }
+        public bool isExistBottomIsland()
+        {
+            if (info.Contains("B"))
+                return true;
+
+            return false;
+        }
+
+        public void DeleteLeft()
+        {
+            info.Remove("L");
+        }
+
+        public void DeleteRight()
+        {
+            info.Remove("R");
+        }
+
+        public void DeleteTop()
+        {
+            info.Remove("T");
+        }
+
+        public void DeleteBottom()
+        {
+            info.Remove("B");
+        }
+
+        public void DeleteFrom(string from)
+        {
+            info.Remove(from);
+        }
     }
 
     public class MaxAreaIsland
     {
-        public int getMaxAreaSize(int[,] input)
+        public void CreateConditionMap(int[][] input, out ItemConditionInfo[,] map)
         {
-            int result = 0;
-
             int row = input.GetLength(0);
-            int col = input.GetLength(1);
+            int col = input[0].GetLength(0);
 
-            for (int i = 0; i < input.GetLength(0); i++)
+            map = new ItemConditionInfo[row, col];
+
+            for (int i = 0; i < row; i++)
             {
-                for (int j = 0; j < input.GetLength(1); j++)
+                for (int j = 0; j < col; j++)
                 {
-                    if (input[i, j] != 0)
+
+                    map[i, j] = new ItemConditionInfo();
+
+                    map[i, j].SetIndexInfo(i, j, input[i][j]);
+
+                    if ((int)map[i, j].info[2] == 0)
                     {
-                        result++;
+                        map[i, j].SetAroundItemExist(false, false, false, false);
+                        continue;
                     }
+
+                    bool isLeft = false;
+                    bool isRight = false;
+                    bool isTop = false;
+                    bool isBottom = false;
+
+                    if (j != 0)
+                        isLeft = input[i][j - 1] == 0 ? false : true;
+                    // check right
+                    if (j + 1 != col)
+                        isRight = input[i][j + 1] == 0 ? false : true;
+                    // check top
+                    if (i != 0)
+                        isTop = input[i - 1][j] == 0 ? false : true;
+                    // check bottom
+                    if (i + 1 != row)
+                        isBottom = input[i + 1][j] == 0 ? false : true;
+
+                    map[i, j].SetAroundItemExist(isLeft, isRight, isTop, isBottom);
                 }
             }
 
-            return result;
         }
 
-        public bool checkPossibleDirection(int[,] input, int rowIndex, int colmIndex, out ItemInfo info)
+        public int MaxAreaOfIsland(int[][] grid)
         {
-            ItemInfo newInfo = new ItemInfo();
+            int maxSize = 0;
+            ItemConditionInfo[,] map;
 
-            // check left
-            if (colmIndex != 0)
-                newInfo.isLeft = input[rowIndex, colmIndex - 1] == 0 ? false : true; 
+            CreateConditionMap(grid, out map);
+
+            List<ItemConditionInfo> island = new List<ItemConditionInfo>();
+
+            for (int rowIndex = 0; rowIndex < grid.GetLength(0); rowIndex++)
+            {
+                for (int colIndex = 0; colIndex < grid[0].GetLength(0); colIndex++)
+                {
+                    island = TrakingIsland(ref map, rowIndex, colIndex);
+                    if (maxSize < island.Count)
+                        maxSize = island.Count;
+                }
+            }
+
+            return maxSize;
+        }
+
+        public List<ItemConditionInfo> TrakingIsland(ref ItemConditionInfo[,] map, int indexRow, int indexCol)
+        {
+            List<ItemConditionInfo> result = new List<ItemConditionInfo>();
+
+            if (map[indexRow, indexCol].isChecked())
+                return result;
+
+            if (!map[indexRow, indexCol].isExistArroundIsland() && map[indexRow, indexCol].getValue() == 0)
+                return result;
+
+            int currentRow = indexRow;
+            int currentCol = indexCol;
+
+            string from = "";
+
+            while (true)
+            {
+                // delete from
+                map[currentRow, currentCol].DeleteFrom(from);
+
+                if (!map[currentRow, currentCol].isChecked())
+                    result.Add(map[currentRow, currentCol]);
+
+                map[currentRow, currentCol].SetItemIsChecked();
+
+                from = "";
+
+                // delete to & update site index
+                // add list & continue
+                if (map[currentRow, currentCol].isExistLeftIsland())
+                {
+                    map[currentRow, currentCol].DeleteLeft();
+
+                    // goto Left
+                    if (!map[currentRow, currentCol - 1].isChecked())
+                    {
+                        from = "R";
+                        currentCol -= 1;
+                    }
+                    continue;
+                }
+                if (map[currentRow, currentCol].isExistRightIsland())
+                {
+                    map[currentRow, currentCol].DeleteRight();
+                    
+                    // goto Right
+                    if (!map[currentRow, currentCol + 1].isChecked())
+                    {
+                        currentCol += 1;
+                        from = "L";
+                    }
+                    continue;
+                }
+                if (map[currentRow, currentCol].isExistTopIsland())
+                {
+                    map[currentRow, currentCol].DeleteTop();
+                    
+                    // goto Top
+                    if (!map[currentRow - 1, currentCol].isChecked())
+                    {
+                        currentRow -= 1;
+                        from = "B";
+                    }
+                    continue;
+                }
+                if (map[currentRow, currentCol].isExistBottomIsland())
+                {
+                    map[currentRow, currentCol].DeleteBottom();
+                    
+                    //goto Bottom
+                    if (!map[currentRow + 1, currentCol].isChecked())
+                    {
+                        from = "T";
+                        currentRow += 1;
+                    }
+                    continue;
+                }
 
 
-            // check right
-            if (colmIndex + 1 != input.GetLength(1))
-                newInfo.isRight = input[rowIndex, colmIndex + 1] == 0 ? false : true;
 
-            // check top
-            if (rowIndex != 0)
-                newInfo.isTop = input[rowIndex - 1, colmIndex] == 0 ? false : true;
+                // check list item have LRTB & continue;
+                bool remain = false;
+                foreach (ItemConditionInfo element in result)
+                {
+                    if (element.isExistArroundIsland())
+                    {
+                        currentRow = element.getRowIndex();
+                        currentCol = element.getColIndex();
 
-            // check bottom
-            if (rowIndex + 1 != input.GetLength(0))
-                newInfo.isBottom = input[rowIndex + 1, colmIndex] == 0 ? false : true;
+                        while (true)
+                        {
+                            if (element.isExistLeftIsland())
+                            {
+                                element.DeleteLeft();
+                                from = "R";
+                                if (!map[currentRow, currentCol - 1].isChecked())
+                                {
+                                    currentCol -= 1;
+                                    remain = true;
+                                    break;
+                                }
+                            }
+                            if (element.isExistRightIsland())
+                            {
+                                element.DeleteRight();
+                                from = "L";
+                                if (!map[currentRow, currentCol + 1].isChecked())
+                                {
+                                    currentCol += 1;
+                                    remain = true;
+                                    break;
+                                }
+                            }
+                            if (element.isExistTopIsland())
+                            {
+                                element.DeleteTop();
+                                from = "B";
+                                if (!map[currentRow - 1, currentCol].isChecked())
+                                {
+                                    currentRow -= 1;
+                                    remain = true;
+                                    break;
+                                }
+                            }
+                            if (element.isExistBottomIsland())
+                            {
+                                element.DeleteBottom();
+                                from = "T";
+                                if (!map[currentRow + 1, currentCol].isChecked())
+                                {
+                                    currentRow += 1;
+                                    remain = true;
+                                    break;
+                                }
+                            }
+                            break;
+                        }
+                        if (remain)
+                            break;
+                    }
+                }
+                if (remain)
+                    continue;
 
-            info = newInfo;
+                break;
+            }
 
-            return info.isLeft | info.isRight | info.isTop | info.isBottom;
+            return result;
         }
     }
 }
--- Island/MaxArea/UnitTestProject1/UnitTest1.cs
+++ Island/MaxArea/UnitTestProject1/UnitTest1.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using MaxArea;
+using System.Collections.Generic;
+using System.Collections;
 
 namespace UnitTestProject1
 {
@@ -16,80 +18,345 @@ namespace UnitTestProject1
         }
 
         [TestMethod]
-        public void getMaxAreaSize1()
+        public void create_itemConditionMap()
         {
-            int[,] input = new int[,] {
-                { 0, 0, 1 },
-                { 0, 0, 0 },
-                { 0, 0, 0 } };
-            int expectedMaxArea = 1;
+            /*
+             *  {0,1,0}
+                {1,1,1}
+                {0,1,0}
 
-            Assert.AreEqual(expectedMaxArea, maxArea.getMaxAreaSize(input));
+                000  | 010B     | 020
+                101R | 111LRTB  | 121L
+                200  | 211T     | 220
+             */
+
+            int[][] input = new int[3][]
+            {
+                new int[] {0,1,0},
+                new int[] {1,1,1},
+                new int[] {0,1,0}
+            };
+
+            ItemConditionInfo[,] expected = new ItemConditionInfo[input.GetLength(0), input[0].GetLength(0)];
+
+            for (int i = 0; i < input.GetLength(0); i++)
+            {
+                for (int j = 0; j < input[0].GetLength(0); j++)
+                {
+                    expected[i, j] = new ItemConditionInfo();
+                }
+            }
+
+            expected[0, 0].SetIndexInfo(0, 0, 0);
+            expected[0, 1].SetIndexInfo(0, 1, 1);
+            expected[0, 2].SetIndexInfo(0, 2, 0);
+
+            expected[1, 0].SetIndexInfo(1, 0, 1);
+            expected[1, 1].SetIndexInfo(1, 1, 1);
+            expected[1, 2].SetIndexInfo(1, 2, 1);
+
+            expected[2, 0].SetIndexInfo(2, 0, 0);
+            expected[2, 1].SetIndexInfo(2, 1, 1);
+            expected[2, 2].SetIndexInfo(2, 2, 0);
+
+            expected[0, 1].SetAroundItemExist(false, false, false, true);
+            expected[1, 0].SetAroundItemExist(false, true, false, false);
+            expected[1, 1].SetAroundItemExist(true, true, true, true);
+            expected[1, 2].SetAroundItemExist(true, false, false, false);
+            expected[2, 1].SetAroundItemExist(false, false, true, false);
+
+            ItemConditionInfo[,] actual;
+
+            maxArea.CreateConditionMap(input, out actual);
+
+            Assert.AreEqual(expected.Length, actual.Length);
+
+            Assert.AreEqual(expected.GetLength(0), actual.GetLength(0));
+            Assert.AreEqual(expected.GetLength(1), actual.GetLength(1));
+
+            Assert.AreEqual(expected[0, 0].info.Count, actual[0, 0].info.Count);
+            Assert.AreEqual(expected[0, 1].info.Count, actual[0, 1].info.Count);
+            Assert.AreEqual(expected[0, 2].info.Count, actual[0, 2].info.Count);
+            Assert.AreEqual(expected[1, 0].info.Count, actual[1, 0].info.Count);
+            Assert.AreEqual(expected[1, 1].info.Count, actual[1, 1].info.Count);
+            Assert.AreEqual(expected[1, 2].info.Count, actual[1, 2].info.Count);
+            Assert.AreEqual(expected[2, 0].info.Count, actual[2, 0].info.Count);
+            Assert.AreEqual(expected[2, 1].info.Count, actual[2, 1].info.Count);
+            Assert.AreEqual(expected[2, 2].info.Count, actual[2, 2].info.Count);
         }
 
+        
 
         [TestMethod]
-        public void getMaxAreaSize2()
+        public void ItemConditionInfo_getRowIndex()
         {
-            int[,] input = new int[,] {
-                { 0, 0, 1 }};
-            int expectedMaxArea = 1;
+            ItemConditionInfo conditionInfo = new ItemConditionInfo();
+            conditionInfo.SetIndexInfo(0, 1, 1);
 
-            Assert.AreEqual(expectedMaxArea, maxArea.getMaxAreaSize(input));
+            Assert.AreEqual(0, conditionInfo.getRowIndex());
         }
 
         [TestMethod]
-        public void 주어진_아이템의_4방향에_위치한_아이템중_1_값이_존재하는지_확인하는_함수1()
+        public void ItemConditionInfo_getColIndex()
         {
-            int[,] input = new int[,] {
-                { 0, 0, 1 },
-                { 0, 1, 1 },
-                { 0, 0, 0 } };
+            ItemConditionInfo conditionInfo = new ItemConditionInfo();
+            conditionInfo.SetIndexInfo(0, 1, 1);
 
-            ItemInfo info;
-            bool result = maxArea.checkPossibleDirection(input, 1, 1, out info);
-
-            Assert.IsTrue(result);
-            Assert.AreEqual(false, info.isLeft);
-            Assert.AreEqual(true, info.isRight);
-            Assert.AreEqual(false, info.isTop);
-            Assert.AreEqual(false, info.isBottom);
+            Assert.AreEqual(1, conditionInfo.getColIndex());
         }
 
         [TestMethod]
-        public void 주어진_아이템의_4방향에_위치한_아이템중_1_값이_존재하는지_확인하는_함수2()
+        public void ItemConditionInfo_getValue()
         {
-            int[,] input = new int[,] {
-                { 1, 0, 1 },
-                { 0, 1, 0 },
-                { 1, 0, 1 } };
+            ItemConditionInfo conditionInfo = new ItemConditionInfo();
+            conditionInfo.SetIndexInfo(0, 1, 1);
 
-            ItemInfo info;
-            bool result = maxArea.checkPossibleDirection(input, 1, 1, out info);
-
-            Assert.IsFalse(result);
-            Assert.AreEqual(false, info.isLeft);
-            Assert.AreEqual(false, info.isRight);
-            Assert.AreEqual(false, info.isTop);
-            Assert.AreEqual(false, info.isBottom);
+            Assert.AreEqual(1, conditionInfo.getValue());
         }
 
         [TestMethod]
-        public void 주어진_아이템의_4방향에_위치한_아이템중_1_값이_존재하는지_확인하는_함수3()
+        public void ItemConditionInfo_isExistArroundIsland()
         {
-            int[,] input = new int[,] {
-                { 0, 0, 1 },
-                { 0, 1, 0 },
-                { 0, 1, 1 } };
+            ItemConditionInfo conditionInfo1 = new ItemConditionInfo();
+            ItemConditionInfo conditionInfo2 = new ItemConditionInfo();
+            
+            conditionInfo1.SetAroundItemExist(false, true, false, false);
+            conditionInfo2.SetAroundItemExist(false, false, false, false);
 
-            ItemInfo info;
-            bool result = maxArea.checkPossibleDirection(input, 1, 1, out info);
+            Assert.AreEqual(true, conditionInfo1.isExistArroundIsland());
+            Assert.AreEqual(false, conditionInfo2.isExistArroundIsland());
+        }
 
-            Assert.IsTrue(result);
-            Assert.AreEqual(false, info.isLeft);
-            Assert.AreEqual(false, info.isRight);
-            Assert.AreEqual(false, info.isTop);
-            Assert.AreEqual(true, info.isBottom);
+        [TestMethod]
+        public void ItemConditionInfo_deleteDirection()
+        {
+            ItemConditionInfo conditionInfo = new ItemConditionInfo();
+            conditionInfo.SetIndexInfo(0, 1, 1);
+            conditionInfo.SetAroundItemExist(true, true, true, true);
+
+            Assert.AreEqual(true, conditionInfo.isExistLeftIsland());
+            Assert.AreEqual(true, conditionInfo.isExistRightIsland());
+            Assert.AreEqual(true, conditionInfo.isExistTopIsland());
+            Assert.AreEqual(true, conditionInfo.isExistBottomIsland());
+
+            conditionInfo.DeleteLeft();
+            Assert.AreEqual(false, conditionInfo.isExistLeftIsland());
+            conditionInfo.DeleteRight();
+            Assert.AreEqual(false, conditionInfo.isExistRightIsland());
+            conditionInfo.DeleteTop();
+            Assert.AreEqual(false, conditionInfo.isExistTopIsland());
+            conditionInfo.DeleteBottom();
+            Assert.AreEqual(false, conditionInfo.isExistBottomIsland());
+        }
+
+        [TestMethod]
+        public void ItemConditionInfo_deleteFrom()
+        {
+            ItemConditionInfo conditionInfo = new ItemConditionInfo();
+            conditionInfo.SetIndexInfo(0, 1, 1);
+            conditionInfo.SetAroundItemExist(true, true, true, true);
+
+            string from = "L";
+
+            conditionInfo.DeleteFrom(from);
+            Assert.AreEqual(false, conditionInfo.isExistLeftIsland());
+        }
+
+        [TestMethod]
+        public void ItemConditionInfo_trackingIsland()
+        {
+            int[][] input = new int[3][]
+            {
+                new int[] {0,1,0},
+                new int[] {1,1,1},
+                new int[] {0,1,0}
+            };
+
+            ItemConditionInfo[,] map;
+            maxArea.CreateConditionMap(input, out map);
+            int indexRow = 0;
+            int indexCol = 1;
+
+            List<ItemConditionInfo> result = maxArea.TrakingIsland(ref map, indexRow, indexCol);
+
+            Assert.AreEqual(5, result.Count);
+        }
+
+        [TestMethod]
+        public void MaxAreaOfIsland_01()
+        {
+            int[][] input = new int[][]
+            {
+                new int[] {1}
+            };
+
+            int result = maxArea.MaxAreaOfIsland(input);
+
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod]
+        public void MaxAreaOfIsland_02()
+        {
+            int[][] input = new int[1][]
+            {
+                new int[] {0,0,0,0,0,0}
+            };
+
+            int result = maxArea.MaxAreaOfIsland(input);
+
+            Assert.AreEqual(0, result);
+        }
+
+        [TestMethod]
+        public void MaxAreaOfIsland_03()
+        {
+            int[][] input = new int[3][]
+            {
+                new int[] {1, 1},
+                new int[] {1, 1},
+                new int[] {1, 0}
+            };
+
+            int result = maxArea.MaxAreaOfIsland(input);
+
+            Assert.AreEqual(5, result);
+        }
+        [TestMethod]
+        public void MaxAreaOfIsland_04()
+        {
+            int[][] input = new int[2][]
+            {
+                new int[] {1, 1, 1, 0},
+                new int[] {1, 1, 1, 1}
+            };
+
+            int result = maxArea.MaxAreaOfIsland(input);
+
+            Assert.AreEqual(7, result);
+        }
+
+        [TestMethod]
+        public void MaxAreaOfIsland_05()
+        {
+            int[][] input = new int[3][]
+            {
+                new int[] {0,1,0},
+                new int[] {1,1,1},
+                new int[] {0,1,0}
+            };
+
+            int result = maxArea.MaxAreaOfIsland(input);
+
+            Assert.AreEqual(5, result);
+        }
+
+        /*
+         
+        {0,0,0,0},
+        {1,1,1,0},
+        {1,1,1,0},
+        {1,1,1,0}
+
+
+        {000,    010,     020,    030},
+        {101RB,  111LRB,  121LB,  130},
+        {201RTB, 211LRTB, 221LTB, 230},
+        {301RT,  311LRT,  321LT,  330}
+
+        00 : skip
+        01 : skip
+        02 : skip
+        03 : skip
+
+        10 : add (from :  ), delete R, jump R
+        11 : add (from : L), delete R, jump R  
+        12 : add (from : L), delete B, jump B
+        22 : add (from : T), delete L, jump L
+        21 : add (from : R), delete L, jump L
+        20 : add (from : R), delete T, jump T >> 10 is already add >> delete B, jump B
+        30 : add (from : T), delete R, jump R
+        31 : add (from : L), delete R, jump R
+        32 : add (from : L), delete T, jump T >> 22 is already add >> 
+
+            check list item condition
+            10B - 11B - 12 - 20 - 21TB - 22B - 30 - 31T - 32
+
+        10 : already added. delete B, jump B(check B is already added). >> 10 is clean >>
+        11 : already added. delete B, jump B(check B is already added). >> 11 is clean >>
+        ...
+        ...
+             */
+
+        [TestMethod]
+        public void MaxAreaOfIsland_06()
+        {
+            int[][] input = new int[5][]
+            {
+                new int[] {1,1,1,0,0},
+                new int[] {0,0,0,0,0},
+                new int[] {1,1,1,0,0},
+                new int[] {1,1,1,0,0},
+                new int[] {1,1,1,0,0}
+            };
+
+            int result = maxArea.MaxAreaOfIsland(input);
+
+            Assert.AreEqual(9, result);
+        }
+
+        [TestMethod]
+        public void MaxAreaOfIsland_07()
+        {
+            int[][] input = new int[5][]
+            {
+                new int[] {1,1,1,0,1},
+                new int[] {1,1,1,0,1},
+                new int[] {1,1,1,0,1},
+                new int[] {1,1,1,0,1},
+                new int[] {1,1,1,0,1}
+            };
+
+            int result = maxArea.MaxAreaOfIsland(input);
+
+            Assert.AreEqual(15, result);
+        }
+
+        [TestMethod]
+        public void MaxAreaOfIsland_08()
+        {
+            int[][] input = new int[5][]
+            {
+                new int[] {1,1,1,0,0},
+                new int[] {0,0,0,0,0},
+                new int[] {0,1,0,0,1},
+                new int[] {1,1,1,0,1},
+                new int[] {0,1,0,0,1}
+            };
+
+            int result = maxArea.MaxAreaOfIsland(input);
+
+            Assert.AreEqual(5, result);
+        }
+
+        [TestMethod]
+        public void MaxAreaOfIsland_09()
+        {
+            int[][] input = new int[8][]
+            {
+                new int[] {0,0,1,0,0,0,0,1,0,0,0,0,0},
+                new int[] {0,0,0,0,0,0,0,1,1,1,0,0,0},
+                new int[] {0,1,1,0,1,0,0,0,0,0,0,0,0},
+                new int[] {0,1,0,0,1,1,0,0,1,0,1,0,0},
+                new int[] {0,1,0,0,1,1,0,0,1,1,1,0,0},
+                new int[] {0,0,0,0,0,0,0,0,0,0,1,0,0},
+                new int[] {0,0,0,0,0,0,0,1,1,1,0,0,0},
+                new int[] {0,0,0,0,0,0,0,1,1,0,0,0,0}
+            };
+
+            int result = maxArea.MaxAreaOfIsland(input);
+
+            Assert.AreEqual(6, result);
         }
     }
 }
